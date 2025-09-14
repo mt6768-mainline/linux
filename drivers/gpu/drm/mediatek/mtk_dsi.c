1919918a880af328074ca54e8dba11998b7d6353
Yassine Oudjana <y.oudjana@protonmail.com>
2025-09-14 19:21:52 +0300

    drm/mediatek: dsi: Register DSI host after preparing everything
    Currently mipi_dsi_host_register is the first thing called on probe,
    making DRM try to access it before anything is prepared. Move registering
    the DSI host to the end to ensure it is accessed only after everything is
    ready.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

--- drivers/gpu/drm/mediatek/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mtk_dsi.c
@@ -1229,18 +1229,10 @@ static int mtk_dsi_probe(struct platform_device *pdev)
 	if (irq_num < 0)
 		return irq_num;
 
-	dsi->host.ops = &mtk_dsi_ops;
-	dsi->host.dev = dev;
-	ret = mipi_dsi_host_register(&dsi->host);
-	if (ret < 0)
-		return dev_err_probe(dev, ret, "Failed to register DSI host\n");
-
 	ret = devm_request_irq(&pdev->dev, irq_num, mtk_dsi_irq,
 			       IRQF_TRIGGER_NONE, dev_name(&pdev->dev), dsi);
-	if (ret) {
-		mipi_dsi_host_unregister(&dsi->host);
+	if (ret)
 		return dev_err_probe(&pdev->dev, ret, "Failed to request DSI irq\n");
-	}
 
 	init_waitqueue_head(&dsi->irq_wait_queue);
 
@@ -1249,6 +1241,13 @@ static int mtk_dsi_probe(struct platform_device *pdev)
 	dsi->bridge.funcs = &mtk_dsi_bridge_funcs;
 	dsi->bridge.of_node = dev->of_node;
 	dsi->bridge.type = DRM_MODE_CONNECTOR_DSI;
+
+	dsi->host.ops = &mtk_dsi_ops;
+	dsi->host.dev = dev;
+
+	ret = mipi_dsi_host_register(&dsi->host);
+	if (ret < 0)
+		return dev_err_probe(dev, ret, "Failed to register DSI host\n");
 
 	return 0;
 }
