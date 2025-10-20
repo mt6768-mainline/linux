0a654f153774f79433afb9c113dedbebf597c22e
rva3 <166127285+rva3@users.noreply.github.com>
2025-10-20 18:01:10 +0300

    dt-bindings: interconnect: mediatek: add header for mt6873

---
+++ include/dt-bindings/interconnect/mediatek,mt6873.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ */
+
+
+#ifndef __DT_BINDINGS_INTERCONNECT_MTK_MT6873_EMI_H
+#define __DT_BINDINGS_INTERCONNECT_MTK_MT6873_EMI_H
+
+#define SLAVE_DDR_EMI		0
+#define MASTER_MCUSYS		1
+#define MASTER_MCU_0		2
+#define MASTER_MCU_1		3
+#define MASTER_MCU_2		4
+#define MASTER_MCU_3		5
+#define MASTER_MCU_4		6
+#define MASTER_GPUSYS		7
+#define MASTER_MMSYS		8
+#define MASTER_MM_VPU		9
+#define MASTER_MM_DISP		10
+#define MASTER_MM_VDEC		11
+#define MASTER_MM_VENC		12
+#define MASTER_MM_CAM		13
+#define MASTER_MM_IMG		14
+#define MASTER_MM_MDP		15
+#define MASTER_VPUSYS		16
+#define MASTER_VPU_0		17
+#define MASTER_VPU_1		18
+#define MASTER_MDLASYS		19
+#define MASTER_MDLA_0		20
+#define MASTER_UFS		21
+#define MASTER_PCIE		22
+#define MASTER_USB		23
+#define MASTER_DBGIF		24
+#define SLAVE_HRT_DDR_EMI	25
+#define MASTER_HRT_MMSYS		26
+#define MASTER_HRT_MM_DISP	27
+#define MASTER_HRT_MM_VDEC	28
+#define MASTER_HRT_MM_VENC	29
+#define MASTER_HRT_MM_CAM	30
+#define MASTER_HRT_MM_IMG	31
+#define MASTER_HRT_MM_MDP	32
+#define MASTER_HRT_DBGIF		33
+#define MASTER_WIFI		34
+#define MASTER_BT		35
+#define MASTER_NETSYS		36
+#define MASTER_HRT_ADSP		37
+#endif
+
